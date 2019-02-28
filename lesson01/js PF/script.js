5db0e5f9f40b58162793a79aac9c6c178ca596a0
wowxenia <40713947+wowxenia@users.noreply.github.com>
2019-02-28 17:30:34 +0300

    commit

--- lesson01/js PF/script.js
+++ lesson01/js PF/script.js
@@ -1,22 +1,23 @@
-let money = prompt("Ваш бюджет на месяц?"),
-    time = prompt('Введите дату в формате YYYY-MM-DD'),
+let money = prompt('Ваш бюджет на месяц?'),
+    time = prompt('Введите дату в формате YYYY-MM-DD');
     question1 = prompt('Введите обязательную статью расходов в этом месяце'),
     question2 = +prompt('Во сколько обойдется?'),
     question3 = prompt('Введите обязательную статью расходов в этом месяце'),
     question4 = +prompt('Во сколько обойдется?');
-    
 
+  
 let appData = {
-  money,
+  budget: money,
   timeData: time,
-  expenses: {[question1] : question2, [question3] : question4},
-  optionalExpenses: "",
+  expenses: {},  
+  optionalExpenses: {},
   income: [''],
   savings: false
 };
 
-let budget = (money - (appData.expenses[question1] + appData.expenses[question3]))/30;
+appData.expenses[question1] = question2;
+appData.expenses[question3] = question4;
 
+alert("Бюджет на один день " + appData.budget/30);
 
 console.log(appData);
-console.log(budget);
