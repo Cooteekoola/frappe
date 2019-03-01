30b1e7e763ff84a59515e2de10108a05e86d28d0
wowxenia <40713947+wowxenia@users.noreply.github.com>
2019-03-01 19:16:15 +0300

    commit

--- lesson01/js PF dop/script_dop.js
+++ lesson01/js PF dop/script_dop.js
@@ -1,4 +1,3 @@
 let num = 33721,
     res = ((num.toString().split("").reduce((accumulator, currentValue) => +accumulator * +currentValue)) ** 3).toString();
-
 alert(res.substr(0, 2));
---
+++ lesson01/js PF/tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+let a = prompt('Введите обязательную статью расходов в этом месяце'),
+    b = +prompt('Во сколько обойдется?'),
+    i = 0;
+// while (i < 2) {    
+//   console.log(i);
+//   i++;
+//   appData.expenses[a] = b;
+//   appData.moneyPerDay = appData.budget/30; 
+// }
+
+do {
+  console.log(i);
+  i++;
+}
+
+while (i < 2);
