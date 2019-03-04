469005ae126dc33632fa09ecfbb0b806c3405606
wowxenia <40713947+wowxenia@users.noreply.github.com>
2019-03-04 23:25:30 +0300

    commit

--- lesson03/dop/script.js
+++ lesson03/dop/script.js
@@ -1,8 +1,52 @@
-String.prototype.firstLetterCaps = function() {
-  return this.charAt(0).toUpperCase() + this.slice(1);
+// function firstLetterCaps(str) {
+//   return str.charAt(0).toUpperCase() + str.slice(1);
+// }
+
+// let str = "урок-3-был слишком легким";
+//     str = firstLetterCaps(str);
+// console.log(str);
+
+// let newStr = str.replace(/-/gi, " ");
+// console.log(newStr);
+
+// let superNewStr = [];
+// superNewStr.push(newStr);
+
+// let arr1 = newStr.split(" ");
+// console.log(arr1);
+
+// let easy = arr1[4],
+//     lastEasy = easy.split("");
+//     lastEasy.splice(4, 5, "о", "о")
+// console.log(lastEasy);
+
+
+
+// let arr = [20, 33, 1, "Человек", 2, 3],
+//     removed = arr.splice(arr.indexOf("Человек"), 1);
+//     sum = 0;
+// console.log(arr);
+// console.log(removed);
+
+// for(let i=0; i < arr.length; i++) {      
+//     sum += Math.pow(arr[i], 3);    
+// }
+
+// console.log(Math.sqrt(sum));
+
+
+function newFunction (x) {
+  if (typeof(x) != "string") {
+    console.log("Это не строка");    
+  } else {
+    x = x.trim(); 
+    if (x.length > 50) {
+      x = x.substring(0, 50) + "...";
+    }
+  } 
+  console.log(x);
 }
 
-let str = "урок-3-был слишком легким";
-    str = str.firstLetterCaps();
-console.log(str);
 
+
+newFunction("     Текст из 50 символов (одно слово - в среднем 6 символов) написан с использованием арабского алфавита мощностью 28 символов. Определить информационный объем текста в битах. Считать, что при наборе текста выбор любого из 28 символов равновероятен.");
--- lesson03/dop/tempCodeRunnerFile.js
+++ lesson03/dop/tempCodeRunnerFile.js
@@ -1,7 +1,4 @@
-String.prototype.firstLetterCaps = function() {
-  return this.charAt(0).toUpperCase() + this.slice(1);
+function newFunction (argument) {
+  console.log("Здесь строка!")
 }
-
-let str = "урок-3-был слишком легким";
-  str = str.firstLetterCaps();
-console.log(str);
+newFunction();
